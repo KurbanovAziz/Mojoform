dc5520ceeb8789d10227f739effc03f3eb029049
Евгений Доленко <dolenkoevgenii@gmail.com>
2017-08-23 19:35:32 +0300

    gradle updated

--- app/src/main/java/org/dev_alex/mojo_qa/mojo/App.java
+++ app/src/main/java/org/dev_alex/mojo_qa/mojo/App.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import android.util.DisplayMetrics;
 
-import com.squareup.leakcanary.LeakCanary;
-
 import java.lang.ref.WeakReference;
 
 public class App extends Application {
@@ -23,14 +21,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-
+        
         mContext = new WeakReference<Context>(this);
         displayMetrics = getResources().getDisplayMetrics();
 
