2ee28058091a1922f8dfb488c2ab27d4fbe7695b
DolenkoEvgenii <dolenkoevgenii@gmail.com>
2019-09-23 16:19:19 +0300

    fix photo attach bug

--- app/src/main/java/org/dev_alex/mojo_qa/mojo/fragments/TemplateFragment.java
+++ app/src/main/java/org/dev_alex/mojo_qa/mojo/fragments/TemplateFragment.java
@@ -3280,12 +3280,12 @@ public class TemplateFragment extends Fragment {
                         Bitmap bitmap = BitmapFactory.decodeFile(picturePath, options);
                         bitmap = BitmapService.modifyOrientation(bitmap, picturePath);
 
+
                         if (!isRestore) {
                             try {
                                 File resFile = new File(picturePath);
                                 tmpOptions = new BitmapFactory.Options();
                                 options = new BitmapFactory.Options();
-
 
                                 tmpOptions.inJustDecodeBounds = true;
                                 BitmapFactory.decodeFile(picturePath, tmpOptions);
@@ -3295,35 +3295,23 @@ public class TemplateFragment extends Fragment {
                                 Bitmap resBitmap = BitmapFactory.decodeFile(picturePath, options);
                                 resBitmap = BitmapService.modifyOrientation(resBitmap, picturePath);
 
-                                ExifInterface exif = new ExifInterface(picturePath);
-                                Date pictureDate = new Date();
-                                try {
-                                    if (!exif.getAttribute(ExifInterface.TAG_DATETIME).isEmpty()) {
-                                        pictureDate = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.getDefault()).parse(exif.getAttribute(ExifInterface.TAG_DATETIME));
-                                    }
-                                } catch (Exception exc) {
-                                    exc.printStackTrace();
-                                }
-
                                 SimpleDateFormat watermarkDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.getDefault());
-                                resBitmap = drawWaterMarkOnBitmap(resBitmap, watermarkDateFormat.format(pictureDate));
+                                resBitmap = drawWaterMarkOnBitmap(resBitmap, watermarkDateFormat.format(new Date()));
 
                                 resFile.delete();
                                 BitmapService.saveBitmapToFile(resFile, resBitmap);
 
-                                try {
-                                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy:MM:dd", Locale.getDefault());
-                                    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                                ExifInterface exif = new ExifInterface(picturePath);
 
-                                    exif.setAttribute(ExifInterface.TAG_DATETIME, dateFormat.format(pictureDate) + " " + timeFormat.format(pictureDate));
-                                    exif.setAttribute(ExifInterface.TAG_EXPOSURE_TIME, dateFormat.format(pictureDate) + " " + timeFormat.format(pictureDate));
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy:MM:dd", Locale.getDefault());
+                                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
-                                    exif.setAttribute(ExifInterface.TAG_GPS_DATESTAMP, dateFormat.format(pictureDate));
-                                    exif.setAttribute(ExifInterface.TAG_GPS_TIMESTAMP, timeFormat.format(pictureDate));
-                                    exif.saveAttributes();
-                                } catch (Exception exc) {
-                                    exc.printStackTrace();
-                                }
+                                exif.setAttribute(ExifInterface.TAG_DATETIME, dateFormat.format(new Date()) + " " + timeFormat.format(new Date()));
+                                exif.setAttribute(ExifInterface.TAG_EXPOSURE_TIME, dateFormat.format(new Date()) + " " + timeFormat.format(new Date()));
+
+                                exif.setAttribute(ExifInterface.TAG_GPS_DATESTAMP, dateFormat.format(new Date()));
+                                exif.setAttribute(ExifInterface.TAG_GPS_TIMESTAMP, timeFormat.format(new Date()));
+                                exif.saveAttributes();
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
