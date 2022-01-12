90b4001f13c004fc2cbe98bf8dca5939db1a0466
Artem <polbuhanky@gmail.com>
2022-01-12 23:45:09 +0300

    Вроде исправил логотипы, проверьте пж

--- app/src/main/java/org/dev_alex/mojo_qa/mojo/App.java
+++ app/src/main/java/org/dev_alex/mojo_qa/mojo/App.java
@@ -35,8 +35,6 @@ public class App extends Application {
 
             case "debug_flavor":
                 host = "https://mojo-qa.dev-alex.org";
-                host = "https://system.mojoform.com";
-
                 break;
 
             case "demo_flavor":
